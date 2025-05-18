f07180dee4f7a7032fc79441a9158b85fd3c2404
LenaSpivakova <Skarlet2710@mail.ru>
2025-05-18 11:10:51 +0300

    Создание таблицы User(ов)

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -5,6 +5,15 @@ import jm.task.core.jdbc.service.UserServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
 
+        userService.saveUser("Alexey", "Sidorov", (byte) 20);
+        userService.saveUser("Maria", "Smirnova", (byte) 23);
+        userService.saveUser("Dmitry", "Suvorov", (byte) 33);
+        userService.saveUser("Petr", "Ivanov", (byte) 25);
+        userService.getAllUsers().forEach(System.out::println);
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
